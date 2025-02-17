1bd9cc6f069c0cd3fc169b860e4f8b8a8c956761
Hoyoung027 <hybyun0207@gmail.com>
2025-02-18 02:56:29 +0900

    [FEAT] 글 읽기 구현

--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 
 # Create your views here.
 
 def item_create(request):
+    
+    # POST 요청이면 사용자의 데이터를 받아 Item 모델에 저장장
     if request.method == "POST" :
         name = request.POST.get('name')
         description = request.POST.get('description')
         item = Item.objects.create(name=name, description=description)
         return redirect('item_create')
     
-    return render(request, 'test_app/item_form.html')
+    # GET 요청이면 폼을 보여줌
+    return render(request, 'item_form.html')
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'item_list.html', {'items':items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'item_detail.html', {'item':item})
+
