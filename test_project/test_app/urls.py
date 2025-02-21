41373f3fcde425e81223e469a332095c693ed2ea
jimin <jm0903@yonsei.ac.kr>
2025-02-21 19:59:15 +0900

    [Troubleshooting] delete pycache

--- test_project/test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.item_create, name='item_create'),
-    path('', views.item_list, name='item_list'),
-    path('<int:pk>/', views.item_detail, name='item_detail'),
-]
